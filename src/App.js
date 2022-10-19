b9d38c44539fa1ca14d596a0a887cd4e90a799e0
kim-seonkyu <seonkyu93@gmail.com>
2022-10-20 00:10:34 +0900

    fixed error

--- src/App.js
+++ src/App.js
@@ -5,11 +5,8 @@ function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
-        <Route
-          path={`${process.env.PUBLIC_URL}/movie/:id`}
-          element={<Detail />}
-        ></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movie/:id" element={<Detail />}></Route>
       </Routes>
     </Router>
   );
